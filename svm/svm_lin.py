dbf86116e133ec12cae525b9160dcaaf6dfc8b1b
Labinot Jakupi <labinotjakupi90@gmail.com>
2017-04-04 22:33:10 +0200

    Plots

--- svm/svm_lin.py
+++ svm/svm_lin.py
@@ -1,8 +1,8 @@
-from sklearn import svm, metrics
-import time
-import numpy as np
-
 from sklearn.model_selection import GridSearchCV
+from sklearn import svm, metrics, utils
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 print('Start time:' + time.ctime())
 
@@ -11,30 +11,57 @@ testSet = np.genfromtxt('test.csv', delimiter=",")
 
 print('Data loaded:' + time.ctime())
 
+# Suffle data
+trainingSet = utils.shuffle(trainingSet)
+testSet = utils.shuffle(testSet)
+
 trainingLabels = trainingSet[:,0]
 trainingData = trainingSet[:,1:]
 
 testLabels = testSet[:,0]
 testData = testSet[:,1:]
 
+row, col = trainingSet.shape
+
+# Plot some original data
+dataAndLabels = list(zip(trainingSet, trainingLabels))
+for index, (image, label) in enumerate(dataAndLabels[:6]):
+    plt.subplot(2, 6, index + 1)
+    plt.axis('off')
+    plt.imshow(trainingSet[index,1:col].reshape((28,28)), cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.title('Training: %i' % label, fontsize=6)
+
+print('Enter SVM...')
+
 classifier = svm.LinearSVC()
+# classifier = svm.LinearSVC(C=0.0001)
 
+	
+# Grid search
 params = {'C': [0.0001, 0.001, 0.1, 1]}
-
 gridSearch = GridSearchCV(classifier, params)
-
 gridSearch.fit(trainingData,trainingLabels)
-
 predictedLabels = gridSearch.predict(testData)
 
-#classifier.fit(trainingData, trainingLabels)
-
-#predictedLabels = classifier.predict(testData)
+# classifier.fit(trainingData, trainingLabels)
+# predictedLabels = classifier.predict(testData)
 
 print('The end:' + time.ctime())
 
-print "C = ", gridSearch.best_estimator_.C
+print ("C = ", gridSearch.best_estimator_.C)
 
 print("Classification report %s:n%sn"
       % (classifier, metrics.classification_report(testLabels, predictedLabels)))
+# Check how many times a digit was recognized as a certain digit.
+print("Confusion matrix:\n %s" % metrics.confusion_matrix(testLabels, predictedLabels))
 
+print("Accuracy:\n %s" % metrics.accuracy_score(testLabels, predictedLabels))
+
+# Plot predicted data
+dataAndPredictions = list(zip(testSet, predictedLabels))
+for index, (image, prediction) in enumerate(dataAndPredictions[:6]):
+    plt.subplot(2, 6, index + 7)
+    plt.axis('off')
+    plt.imshow(testSet[index,1:col].reshape((28,28)), cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.title('Prediction: %i' % prediction, fontsize=6)
+plt.show()
--- svm/svm_rbf.py
+++ svm/svm_rbf.py
@@ -1,12 +1,12 @@
 import time
 import numpy as np
 
-from sklearn import svm, metrics
+import matplotlib.pyplot as plt
+from sklearn import svm, metrics, utils
 from numpy import genfromtxt
 from skimage.feature import hog
 from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
-
 
 print('Start time:' + time.ctime())
 
@@ -15,15 +15,32 @@ testSet = genfromtxt('test.csv', delimiter=",")
 
 print('Data loaded:' + time.ctime())
 
+# Suffle data
+trainingSet = utils.shuffle(trainingSet)
+testSet = utils.shuffle(testSet)
+
 trainingLabels = trainingSet[:,0]
 trainingData = trainingSet[:,1:]
+
 #trainingData_scaled = preprocessing.scale(trainingData)
 min_max_scaler = preprocessing.MinMaxScaler()
 sc_trainingData = min_max_scaler.fit_transform(trainingData)
 
 testLabels = testSet[:,0]
 testData = testSet[:,1:]
+
 sc_testData = min_max_scaler.transform(testData)
+
+
+row, col = trainingSet.shape
+# Plot some original data
+dataAndLabels = list(zip(trainingSet, trainingLabels))
+for index, (image, label) in enumerate(dataAndLabels[:6]):
+    plt.subplot(2, 6, index + 1)
+    plt.axis('off')
+    plt.imshow(trainingSet[index,1:col].reshape((28,28)), cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.title('Training: %i' % label, fontsize=6)
+
 
 # # HoG features for training data
 # hogFeaturesList = []
@@ -31,7 +48,8 @@ sc_testData = min_max_scaler.transform(testData)
 #     fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
 #     hogFeaturesList.append(fd)
 # hogFeatures = np.array(hogFeaturesList, 'float64')
-#
+
+
 # # HoG features for test data
 # hog_ft = []
 # for feature in testData:
@@ -39,14 +57,13 @@ sc_testData = min_max_scaler.transform(testData)
 #     hog_ft.append(ft)
 # hogTestFeatures = np.array(hog_ft, 'float64')
 
-
 '''
 We performed grid search to find the optimal values for parameters C and gamma
 from the list that we tested(C=5, gamma=0.05 were the best).
 Grid search is very time consuming so we will comment it out
 '''
 
-svc = svm.SVC(kernel='rbf', cache_size=800, C=5, gamma=0.05)
+svc = svm.SVC(kernel='rbf', cache_size=1000, C=5, gamma=0.05)
 
 svc.fit(sc_trainingData, trainingLabels)
 
@@ -64,3 +81,17 @@ print('The end:' + time.ctime())
 #print "Best params:", gridSearch.best_params_
 
 print("Classification report %s:n%sn" % (svc, metrics.classification_report(testLabels, predictedLabels)))
+
+# Check how many times a digit was recognized as a certain digit.
+print("Confusion matrix:\n %s" % metrics.confusion_matrix(testLabels, predictedLabels))
+
+print("Accuracy:\n %s" % metrics.accuracy_score(testLabels, predictedLabels))
+
+# Plot predicted data
+dataAndPredictions = list(zip(testSet, predictedLabels))
+for index, (image, prediction) in enumerate(dataAndPredictions[:6]):
+    plt.subplot(2, 6, index + 7)
+    plt.axis('off')
+    plt.imshow(testSet[index,1:col].reshape((28,28)), cmap=plt.cm.Greys_r, interpolation='nearest')
+    plt.title('Prediction: %i' % prediction, fontsize=6)
+plt.show()
