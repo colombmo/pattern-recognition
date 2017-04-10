3c6dc6e049719adcfc4d31ea03b649e4e5e47177
Egzon Syka <egzon.syka@students.unibe.ch>
2017-04-10 23:44:01 +0200

    Comment formatting

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -4,7 +4,6 @@ import numpy as np
 import time
 
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import StandardScaler
 
 print('Start time:' + time.ctime())
 
@@ -27,20 +26,19 @@ sc_testData = min_max_scaler.transform(testData)
 clf = MLPClassifier(solver='sgd', activation='relu', learning_rate='constant', learning_rate_init=0.3,
                     hidden_layer_sizes=(100,), alpha=0.01)
 
-# print 'Params:',clf.get_params().keys()
+# To apply cross validation uncomment the code below and remove those
+# parameters from mlpclassifier above
 
-lr_range = np.linspace(0.1, 1, 10)
-neurons_range = np.linspace(10, 100, 10)
-iter_range = np.linspace(100, 200, 3)
+# lr_range = np.linspace(0.1, 1, 10)
+# neurons_range = np.linspace(10, 100, 10)
 
 # Grid search
 # params = {'learning_rate_init': lr_range, 'hiden_layer_sizes':[(10,),(25,),(50,),(75,),(100,)]}
 # params = {'solver':['sgd','adam'],'activation':['identity', 'logistic', 'tanh', 'relu']}
-# params = {'hidden_layer_sizes': [(70,), (75,), (80,), (100,)]}
-#params = {'alpha':[1e-2, 1e-3, 1e-4, 1e-5]}
-#gridSearch = GridSearchCV(clf, params, n_jobs=-1)
-#gridSearch.fit(sc_trainingData, trainingLabels)
-#predictedLabels = gridSearch.predict(sc_testData)
+# params = {'alpha':[1e-2, 1e-3, 1e-4, 1e-5]}
+# gridSearch = GridSearchCV(clf, params, n_jobs=-1)
+# gridSearch.fit(sc_trainingData, trainingLabels)
+# predictedLabels = gridSearch.predict(sc_testData)
 
 clf.fit(sc_trainingData, trainingLabels)
 predictedLabels = clf.predict(sc_testData)
